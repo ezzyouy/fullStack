a8f9d0d554a338bfa6465d00513850a7bd3c4ebd
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-10-26 16:38:08 +0100

    add bootstrap and axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import data from './data'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App () {
   return (
     <BrowserRouter>
-      <div className='App'>
-        <header className='App-header'>
-          <Link to='/'>Amazona</Link>
+      <div className='d-flex flex-column site-container'>
+        <header>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>Amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/product/:_id' element={<ProductScreen />} />
-            <Route path='/' element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/product/:_id' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   )
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,28 +1,67 @@
-import React from 'react'
-import data from '../data'
+import React, { useEffect, useReducer } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import logger from 'use-reducer-logger'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...setTimeout, loading: true }
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload }
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
 
 export default function HomeScreen () {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: ''
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const result = await axios.get('/api/products/')
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message })
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
       <h1>Featured Produts</h1>
       <div className='products'>
-        {data.products.map(product => (
-          <div className='product' key={product._id}>
-            <Link to={`/product/${product._id}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className='product-info'>
+        {loading ? (
+          <div>loading</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products?.map(product => (
+            <div className='product' key={product._id}>
               <Link to={`/product/${product._id}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
+              <div className='product-info'>
+                <Link to={`/product/${product._id}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   )
