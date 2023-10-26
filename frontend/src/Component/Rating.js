c0993484306cab2d49f348edb2ccba75309515bd
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-10-26 23:26:29 +0100

    rating&numReviews

---
+++ frontend/src/Component/Rating.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons'
+import { faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+library.add(fasFaStar, farFaStar, faStarHalfAlt)
+
+function Rating (props) {
+  const { rating, numReviews } = props
+  return (
+    <div className='rating'>
+      <span>
+        <FontAwesomeIcon
+          icon={
+            rating >= 1 ? fasFaStar : rating >= 0.5 ? faStarHalfAlt : farFaStar
+          }
+        />
+      </span>
+
+      <span>
+        <FontAwesomeIcon
+          icon={
+            rating >= 2 ? fasFaStar : rating >= 1.5 ? faStarHalfAlt : farFaStar
+          }
+        />
+      </span>
+
+      <span>
+        <FontAwesomeIcon
+          icon={
+            rating >= 3 ? fasFaStar : rating >= 2.5 ? faStarHalfAlt : farFaStar
+          }
+        />
+      </span>
+
+      <span>
+        <FontAwesomeIcon
+          icon={
+            rating >= 4 ? fasFaStar : rating >= 3.5 ? faStarHalfAlt : farFaStar
+          }
+        />
+      </span>
+
+      <span>
+        <FontAwesomeIcon
+          icon={
+            rating >= 5 ? fasFaStar : rating >= 4.5 ? faStarHalfAlt : farFaStar
+          }
+        />
+      </span>
+      <span> {numReviews} reciews</span>
+    </div>
+  )
+}
+
+export default Rating
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useReducer } from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
 import logger from 'use-reducer-logger'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Product from '../Component/Product'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -45,22 +47,13 @@ export default function HomeScreen () {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products?.map(product => (
-            <div className='product' key={product._id}>
-              <Link to={`/product/${product._id}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className='product-info'>
-                <Link to={`/product/${product._id}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products?.map(product => (
+              <Col key={product._id} sm={6} md={4} lg={3} className='mb-3'>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
