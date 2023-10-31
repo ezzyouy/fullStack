5d586ee65342b1cbe235edd396e1aa95d47a66fc
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-10-31 18:23:15 +0100

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import { StoreProvider } from './Store';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	//<React.StrictMode>
