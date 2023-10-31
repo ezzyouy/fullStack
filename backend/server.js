e5fd8e407a640b0814d1220bb5a7293e029bc82f
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-10-31 11:22:47 +0100

    Pay-Order-PayPal

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,40 @@
-import express from 'express'
-import data from './data.js'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import seedRouter from './routes/seedRoutes.js'
-import productRouter from './routes/productRoutes.js'
-import userRouter from './routes/userRoutes.js'
-import orderRouter from './routes/OrderRoutes.js'
+import express from 'express';
+import data from './data.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/OrderRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('connected to Mongodb')
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('connected to Mongodb');
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/seed', seedRouter)
-app.use('/api/products', productRouter)
-app.use('/api/users', userRouter)
-app.use('/api/orders', orderRouter)
+app.use('/api/keys/paypal', (req, res) => {
+	res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message })
-})
-const port = process.env.PORT || 5000
+	res.status(500).send({ message: err.message });
+});
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server started on ${port}`)
-})
+	console.log(`Server started on ${port}`);
+});
