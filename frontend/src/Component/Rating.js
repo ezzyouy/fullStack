a7c6521ce768e154b3cdd1117fdec5b2bc7ae1e6
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-11-02 10:52:28 +0100

    Create-sidebar-SearchCreate-Search-ScreenBox

--- frontend/src/Component/Rating.js
+++ frontend/src/Component/Rating.js
@@ -1,57 +1,37 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons'
-import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons'
-import { faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
-library.add(fasFaStar, farFaStar, faStarHalfAlt)
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons';
+import { faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+library.add(fasFaStar, farFaStar, faStarHalfAlt);
 
-function Rating (props) {
-  const { rating, numReviews } = props
-  return (
-    <div className='rating'>
-      <span>
-        <FontAwesomeIcon
-          icon={
-            rating >= 1 ? fasFaStar : rating >= 0.5 ? faStarHalfAlt : farFaStar
-          }
-        />
-      </span>
+function Rating(props) {
+	const { rating, numReviews, caption } = props;
+	return (
+		<div className="rating">
+			<span>
+				<FontAwesomeIcon icon={rating >= 1 ? fasFaStar : rating >= 0.5 ? faStarHalfAlt : farFaStar} />
+			</span>
 
-      <span>
-        <FontAwesomeIcon
-          icon={
-            rating >= 2 ? fasFaStar : rating >= 1.5 ? faStarHalfAlt : farFaStar
-          }
-        />
-      </span>
+			<span>
+				<FontAwesomeIcon icon={rating >= 2 ? fasFaStar : rating >= 1.5 ? faStarHalfAlt : farFaStar} />
+			</span>
 
-      <span>
-        <FontAwesomeIcon
-          icon={
-            rating >= 3 ? fasFaStar : rating >= 2.5 ? faStarHalfAlt : farFaStar
-          }
-        />
-      </span>
+			<span>
+				<FontAwesomeIcon icon={rating >= 3 ? fasFaStar : rating >= 2.5 ? faStarHalfAlt : farFaStar} />
+			</span>
 
-      <span>
-        <FontAwesomeIcon
-          icon={
-            rating >= 4 ? fasFaStar : rating >= 3.5 ? faStarHalfAlt : farFaStar
-          }
-        />
-      </span>
+			<span>
+				<FontAwesomeIcon icon={rating >= 4 ? fasFaStar : rating >= 3.5 ? faStarHalfAlt : farFaStar} />
+			</span>
 
-      <span>
-        <FontAwesomeIcon
-          icon={
-            rating >= 5 ? fasFaStar : rating >= 4.5 ? faStarHalfAlt : farFaStar
-          }
-        />
-      </span>
-      <span> {numReviews} reciews</span>
-    </div>
-  )
+			<span>
+				<FontAwesomeIcon icon={rating >= 5 ? fasFaStar : rating >= 4.5 ? faStarHalfAlt : farFaStar} />
+			</span>
+			{caption ? <span>{caption}</span> : <span> {' ' + numReviews + ' reviews'} </span>}
+		</div>
+	);
 }
 
-export default Rating
+export default Rating;
---
+++ frontend/src/Screens/SearchScreen.js
@@ -0,0 +1,253 @@
+import React, { useEffect, useReducer, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { getError } from '../utils';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { Helmet } from 'react-helmet-async';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Rating from '../Component/Rating';
+import LoadingBox from '../Component/LoadingBox';
+import MessageBox from '../Component/MessageBox';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import Product from '../Component/Product';
+import { LinkContainer } from 'react-router-bootstrap';
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'FETCH_REQUEST':
+			return { ...state, loading: true };
+
+		case 'FETCH_SUCCESS':
+			return {
+				...state,
+				loading: false,
+				products: action.payload.products,
+				page: action.payload.page,
+				countProducts: action.payload.countProducts,
+				pages: action.payload.pages,
+			};
+
+		case 'FETCH_FAIL':
+			return { ...state, loading: false, error: action.payload };
+		default:
+			return state;
+	}
+};
+const prices = [
+	{ name: '$1 to $50', value: '1-50' },
+	{ name: '$51 to $200', value: '51-200' },
+	{ name: '$201 to $1000', value: '201-1000' },
+];
+export const ratings = [
+	{ name: '4stars & up', rating: 4 },
+	{ name: '3stars & up', rating: 3 },
+	{ name: '2stars & up', rating: 2 },
+	{ name: '1stars & up', rating: 1 },
+];
+function SearchScreen() {
+	const navigate = useNavigate();
+	const { search } = useLocation();
+	const sp = new URLSearchParams(search);
+	const category = sp.get('category') || 'all';
+	const query = sp.get('query') || 'all';
+	const price = sp.get('price') || 'all';
+	const rating = sp.get('rating') || 'all';
+	const order = sp.get('order') || 'all';
+	const page = sp.get('page') || 'all';
+
+	const [{ loading, error, products, pages, countProducts }, dispatch] = useReducer(reducer, {
+		loading: true,
+		error: '',
+	});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				dispatch({ type: 'FETCH_REQUEST' });
+				const { data } = await axios.get(
+					`/api/products/search?page=${page}&query=${query}&category=${category}&price=${price}&rating=${rating}&order=${order}`
+				);
+				dispatch({ type: 'FETCH_SUCCESS', payload: data });
+			} catch (err) {
+				dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+			}
+		};
+		fetchData();
+	}, [category, order, page, pages, query, rating]);
+
+	const [categories, setCategories] = useState([]);
+	useEffect(() => {
+		const fetchCategories = async () => {
+			try {
+				const { data } = await axios.get(`/api/products/categories`);
+				setCategories(data);
+			} catch (err) {
+				toast.error(getError(err));
+			}
+		};
+		fetchCategories();
+	}, []);
+
+	const getFilterUrl = (filter) => {
+		const filterPage = filter.page || page;
+		const filterCategory = filter.category || category;
+		const filterQuery = filter.query || query;
+		const filterRating = filter.rating || rating;
+		const filterPrice = filter.price || price;
+		const sortOrder = filter.order || order;
+		const to = `/search?category=${filterCategory}&query=${filterQuery}&rating=${filterRating}&price=${filterPrice}&rating=${rating}&order=${sortOrder}&page=${filterPage}`;
+		const url = new URL(to, window.location.origin);
+		const path = {
+			hash: url.hash,
+			pathname: url.pathname,
+			search: url.search,
+		};
+		return path;
+	};
+
+	return (
+		<div>
+			<Helmet>
+				<title>Search Products</title>
+			</Helmet>
+			<Row>
+				<Col md={3}>
+					<h3>Departement</h3>
+					<div>
+						<ul>
+							<li>
+								<Link
+									className={'all' === category ? 'text-bold' : ''}
+									to={getFilterUrl({ category: 'all' })}
+								>
+									Any
+								</Link>
+							</li>
+							{categories?.map((c) => (
+								<li key={c}>
+									<Link
+										className={c === category ? 'text-bold' : ''}
+										to={getFilterUrl({ category: c })}
+									>
+										{c}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div>
+						<h3>Price</h3>
+						<ul>
+							<li>
+								<Link
+									className={'all' === price ? 'text-bold' : ''}
+									to={getFilterUrl({ price: 'all' })}
+								>
+									Any
+								</Link>
+							</li>
+							{prices?.map((p) => (
+								<li key={p}>
+									<Link
+										className={p.value === price ? 'text-bold' : ''}
+										to={getFilterUrl({ price: p.value })}
+									>
+										{p.name}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div>
+						<h3>Avg. Custome Review</h3>
+						<ul>
+							{console.log(rating)}
+							{ratings?.map((r) => (
+								<li key={r.name}>
+									<Link
+										to={getFilterUrl({ rating: r.rating })}
+										className={`${r.rating}` === `${rating}` ? 'text-bold' : ''}
+									>
+										<Rating caption={' & up'} rating={r.rating}></Rating>
+									</Link>
+								</li>
+							))}
+							<li>
+								<Link
+									to={getFilterUrl({ rating: 'all' })}
+									className={rating === 'all' ? 'text-bold' : ''}
+								>
+									<Rating caption={' & up'} rating={0}></Rating>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</Col>
+				<Col md={9}>
+					{loading ? (
+						<LoadingBox></LoadingBox>
+					) : error ? (
+						<MessageBox variant="danger">{error}</MessageBox>
+					) : (
+						<>
+							<Row className=" justify-content-between  mb-3">
+								<Col md={6}>
+									<div>
+										{countProducts === 0 ? 'No' : countProducts} Results
+										{query !== 'all' && ' : ' + query}
+										{category !== 'all' && ' : ' + category}
+										{price !== 'all' && ' : ' + price}
+										{rating !== 'all' && ' : ' + rating + ' & up'}
+										{query !== 'all' ||
+										category !== 'all' ||
+										rating !== 'all' ||
+										price !== 'all' ? (
+											<Button variant="light" onClick={() => navigate('/search')}>
+												<FontAwesomeIcon icon={faTimesCircle} />
+											</Button>
+										) : null}
+									</div>
+								</Col>
+								<Col className="text-end">
+									Sort by{' '}
+									<select
+										value={order}
+										onChange={(e) => {
+											navigate(getFilterUrl({ order: e.target.value }));
+										}}
+									>
+										<option value="newest">Newest Arrivals</option>
+										<option value="lowest">Price: Low to High</option>
+										<option value="highest">Price: High to Low</option>
+										<option value="toprated">Avg. Customer Reviews</option>
+									</select>
+								</Col>
+							</Row>
+							{products?.length === 0 && <MessageBox>No Product Found</MessageBox>}
+							<Row>
+								{products?.map((product) => (
+									<Col sm={6} lg={4} className="mb-3" key={product._id}>
+										<Product product={product}></Product>
+									</Col>
+								))}
+							</Row>
+							<div>
+								{[...Array(pages).keys()]?.map((x) => (
+									<LinkContainer key={x + 1} className="mx-1" to={getFilterUrl({ page: x + 1 })}>
+										<Button className={Number(page) === x + 1 ? 'text-bold' : ''} variant="light">
+											{x + 1}
+										</Button>
+									</LinkContainer>
+								))}
+							</div>
+						</>
+					)}
+				</Col>
+			</Row>
+		</div>
+	);
+}
+
+export default SearchScreen;
